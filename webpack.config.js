152b80d5510ef6f6371c00fd319368aa518d17f3
Lars Selle <lars.selle.dev@gmail.com>
2017-10-04 22:09:21 +0200

    change webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 let libraryName = 'ela';
 
@@ -11,5 +12,39 @@ module.exports = {
     library: libraryName,
     libraryTarget: 'umd',
     umdNamedDefine: true
-  }
+  },
+
+  devtool: false,
+
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: {
+            loader: 'css-loader',
+            options: {minimize: true}
+          }
+        })
+      }
+    ]
+  },
+
+  plugins: [
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new ExtractTextPlugin({
+      filename: libraryName + '-ui.css',
+      disable: false,
+      allChunks: true
+    })
+  ]
 };
