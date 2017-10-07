eb0ec36168d5623a48fa8df60f74ed422689d324
Lars Selle <lars.selle.dev@gmail.com>
2017-10-07 14:54:12 +0200

    refactored custom-element, button and text-field

--- src/elements/custom-element.js
+++ src/elements/custom-element.js
@@ -4,28 +4,30 @@ export class CustomElement extends HTMLElement{
   }
 
   attributeChangedCallback(name, oldValue, newValue) {
-    if (typeof this[name] === 'boolean') {
-      if (newValue === '' && this[name] === false)
-        this[name] = true;
-    }
-    else {
-      if (this[name] !== newValue)
-        this[name] = newValue;
-    }
+    if (typeof this[name] === 'boolean')
+      newValue = this[name];
+    notifyValueChanged(this, name, newValue, oldValue);
   }
 
-  _setAttribute(name, value) {
+  _setValue(name, value) {
     if (value) {
       if (this.getAttribute(name) !== value) {
-        if (typeof wert === 'boolean')
+        if (typeof value === 'boolean')
           this.setAttribute(name, '');
         else
           this.setAttribute(name, value);
       }
     }
     else {
-      if (this.hasAttribute(name))
+      if (this.hasAttribute(name)) {
         this.removeAttribute(name);
+      }
     }
   }
-}
+};
+
+function notifyValueChanged(object, name, newValue, oldValue) {
+  let fn = `on${name.charAt(0).toUpperCase()}${name.slice(1)}Changed`;
+  if (typeof object[fn] === 'function')
+    object[fn](newValue, oldValue);
+};
